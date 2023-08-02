77af0b3dafd0811697373d06267900030ded16f4
jaifiresong <jaifiresong@gmail.com>
2023-08-02 15:31:35 +0800

    add rsa

--- tests/Crypto/RSATest.php
+++ tests/Crypto/RSATest.php
@@ -12,11 +12,11 @@ class RSATest extends TestCase
     public function test01()
     {
         //openssl_get_privatekey 和 openssl_pkey_get_private 都用于获取私钥，前者在失败时会抛出错误，后者则返回 false。
-        $pub = file_get_contents(__DIR__ . '/cert/public.key');
+        $pub = file_get_contents(__DIR__ . '/cert/public_key.pem');
         $pub = openssl_get_publickey($pub);
         var_dump($pub);
 
-        $pri = file_get_contents(__DIR__ . '/cert/private.key');
+        $pri = file_get_contents(__DIR__ . '/cert/private_key.pem');
         $pri = openssl_pkey_get_private($pri);
         var_dump($pri);
         $this->assertIsString('');
@@ -24,8 +24,8 @@ class RSATest extends TestCase
 
     public function test02()
     {
-        $pub = file_get_contents(__DIR__ . '/cert/public.key');
-        $pri = file_get_contents(__DIR__ . '/cert/private.key');
+        $pub = file_get_contents(__DIR__ . '/cert/public_key.pem');
+        $pri = file_get_contents(__DIR__ . '/cert/private_key.pem');
         $txt = '未未未未未未未未未未';
         $txt .= '担担担担担担担担担担';
         $txt .= '任任任任任任任任任任';
@@ -52,8 +52,8 @@ class RSATest extends TestCase
 
     public function test03()
     {
-        $pub = file_get_contents(__DIR__ . '/cert/public.key');
-        $pri = file_get_contents(__DIR__ . '/cert/private.key');
+        $pub = file_get_contents(__DIR__ . '/cert/public_key.pem');
+        $pri = file_get_contents(__DIR__ . '/cert/private_key.pem');
 
         $msg = 'PHPUnit 8.5.33 by Sebastian Bergmann and contributors.';
         $sign = RSA::sign($msg, $pri);
