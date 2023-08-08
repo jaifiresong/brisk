1f65cd3cf9652624bf56768523330652e98f71b4
jaifiresong <jaifiresong@gmail.com>
2023-08-08 14:53:16 +0800

    up AES

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+
+
+\Jaifire\Brisk\File\Stream::download(__FILE__);
--- src/File/Stream.php
+++ src/File/Stream.php
@@ -7,20 +7,21 @@ class Stream
 {
     /**
      * 在浏览器中下载文件
-     * @param string $file_path 原文件
-     * @param string $name 下载名称
+     * @param $file_path
+     * @param $name
      */
     public static function download($file_path, $name = null)
     {
-        if (empty($name)) {
+        if (is_null($name)) {
             $name = mb_convert_encoding(urldecode(basename($file_path)), "gb2312", "utf-8");
         }
+        $file_path = mb_convert_encoding(urldecode($file_path), "gb2312", "utf-8");
+        $content = file_get_contents($file_path);
         header("Content-type: application/octet-stream");
         header("Accept-Ranges: bytes");
-        header("Accept-Length: " . strlen($file_path));
+        header("Accept-Length: " . strlen($content));
         header("Content-Disposition: attachment; filename=$name");
-        $file_path = mb_convert_encoding(urldecode($file_path), "gb2312", "utf-8");
-        echo file_get_contents($file_path);
+        echo $content;
         exit();
     }
 }
